0215293783d6084cbb85a5ae63b5104194a2e667
Armando Galván <zareek99@gmail.com>
2015-09-12 14:41:27 -0500

    Initial Commit

---
+++ robotgasolina/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  robotgasolina
+//
+//  Created by Zareek Galvan on 9/12/15.
+//  Copyright (c) 2015 Trisquel Labs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
